6057f728f373784964d386e3ded960f48c678bbe
Hjörtur Hjartarson <hjoertur.hjartarson@daimler.com>
2018-08-29 18:46:44 +0200

    Removed awfully large slider component

--- app/views/VesicleAnalyzer/components/Image.js
+++ app/views/VesicleAnalyzer/components/Image.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { compose } from 'recompose';
 
 import withStyles from '@material-ui/core/styles/withStyles';
-import Slider from 'react-compound-slider'
+import Slider from '@material-ui/lab/Slider';
 import withLoadingSpinner from '../../../utils/with/loadingSpinner';
 
 import imageStyle from '../styles/imageStyle';
-import { Rail, Handle, Track } from './slider'
 
 type Props = {
   classes: object,
@@ -39,47 +38,6 @@ class Image extends Component<Props> {
     return (
       <React.Fragment>
         <img src={`data:image/png;base64,${imgData}`} alt="original" />
-        <Slider
-          mode={2}
-          step={1}
-          domain={domain}
-          className={classes.slider}
-          onUpdate={this.onUpdate}
-          onChange={this.onChange}
-          values={values}
-        >
-          <Slider.Rail>
-            {({ getRailProps }) => <Rail getRailProps={getRailProps} />}
-          </Slider.Rail>
-          <Slider.Handles>
-            {({ handles, getHandleProps }) => (
-              <div>
-                {handles.map(handle => (
-                  <Handle
-                    key={handle.id}
-                    handle={handle}
-                    domain={domain}
-                    getHandleProps={getHandleProps}
-                  />
-                ))}
-              </div>
-            )}
-          </Slider.Handles>
-          <Slider.Tracks right={false}>
-            {({ tracks, getTrackProps }) => (
-              <div>
-                {tracks.map(({ id, source, target }) => (
-                  <Track
-                    key={id}
-                    source={source}
-                    target={target}
-                    getTrackProps={getTrackProps}
-                  />
-                ))}
-              </div>
-            )}
-          </Slider.Tracks>
-        </Slider>
       </React.Fragment>
     );
   }
