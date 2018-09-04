9300339f8295d2949924f7e26085792ce6d77bc6
Hjörtur Hjartarson <hjoertur.hjartarson@daimler.com>
2018-09-04 17:59:33 +0200

    Major refactoring

--- app/views/VesicleAnalyzer/containers/ImageContainer.js
+++ app/views/VesicleAnalyzer/containers/ImageContainer.js
@@ -1,228 +1,110 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/lab/Slider';
 // import Slider from '@material-ui/lab/Slider';
 
 import imageStyle from '../styles/imageStyle';
 import Image from '../components/Image';
-
-import CustomSpinner from '../../../components/Progress/CustomSpinner';
+import {
+  paramsDetectCircles,
+  paramsDataProcessing,
+  defaultParams
+} from '../../../variables/paramControl';
 
 type Props = {
   classes: object,
-  res: object
+  loading: object,
+  originalImg: string,
+  processedImg: string,
+  detectedImg: string,
+  currentlySelectedData: object,
+  changeParams: object
 };
 
-const defaultParams = {
-  minBinaryThreshold: 40,
-  maxBinaryThreshold: 100,
-  gaussianBlur: 0,
-  dp: 2.4,
-  centerDistance: 40,
-  minRadius: 10,
-  maxRadius: 80,
-}
-
 class ImageContainer extends Component<Props> {
+  generateImageBox = (
+    imageData,
+    title,
+    isLoading,
+    classes,
+    parameterControl,
+    changeParams,
+    params
+  ) => (
+    <div className={classes.imageBox}>
+      <h1>Original</h1>
+      <div className={classes.image}>
+        <Image imgData={imageData} isLoading={isLoading} />
+      </div>
+      {parameterControl &&
+        parameterControl.map(entry =>
+          this.generateParameterControl(entry, classes, changeParams, params)
+        )}
+    </div>
+  );
+
+  generateParameterControl = (entry, classes, changeParams, params) => (
+    <div className={classes.parameterControl}>
+      <span className={classes.parameterControlText}>{entry.name}</span>
+      <Slider
+        min={entry.min}
+        max={entry.max}
+        className={classes.parameterControlSlider}
+        value={params[entry.variable]}
+        aria-labelledby="label"
+        onChange={(event, value) =>
+          changeParams({
+            [entry.variable]: value
+          })
+        }
+      />
+      <span className={classes.parameterControlValue}>
+        {params[entry.variable]}
+      </span>
+    </div>
+  );
+
   render() {
     const {
-      res,
       classes,
-      onClickLoad,
-      loadingOriginal,
-      loadingProcessed,
-      loadingDetectedCircles,
+      loading,
       originalImg,
       processedImg,
       detectedImg,
-      onClickProcess,
-      onClickDetect,
-      currentlySelectedData
+      currentlySelectedData,
+      changeParams
     } = this.props;
 
-    let params; let condition; let position; let time = null; null; null; null;
+    let params = null;
+
     if (currentlySelectedData !== null) {
-      params = currentlySelectedData.params
-      position = currentlySelectedData.position
-      condition = currentlySelectedData.condition
-      time = currentlySelectedData.time
+      params = currentlySelectedData.params;
     } else {
-      params = defaultParams
-      position = null,
-      condition = null,
-      time = null
+      params = defaultParams;
     }
     return (
       <div className={classes.imageContainer}>
-        <div className={classes.imageBox}>
-          <h1>Original</h1>
-          <div className={classes.image}>
-            {originalImg ? (
-              <Image imgData={originalImg} />
-            ) : (
-              loadingOriginal && <CustomSpinner message="Loading image" />
-            )}
-          </div>
-          <Button
-            onClick={onClickLoad}
-            className={classes.button}
-            variant="contained"
-            color="primary"
-          >
-            {' '}
-            Load image{' '}
-          </Button>
-        </div>
-        <div className={classes.imageBox}>
-          <h1>Processed</h1>
-          <div className={classes.image}>
-            {processedImg ? (
-              <Image imgData={processedImg} />
-            ) : (
-              loadingProcessed && (
-                <CustomSpinner message="Processing image..." />
-              )
-            )}
-          </div>
-          <div className={classes.parameterControl}>
-            <span className={classes.parameterControlText}>
-              Binary Threshold (min):
-            </span>
-            <Slider
-              min={0}
-              max={255}
-              className={classes.parameterControlSlider}
-              value={params.minBinaryThreshold}
-              aria-labelledby="label"
-              onChange={(event,value) => this.props.changeParams(condition, position, time, {minBinaryThreshold:value})}
-            />
-            <span className={classes.parameterControlValue}>
-              {params.minBinaryThreshold}
-            </span>
-          </div>
-          <div className={classes.parameterControl}>
-            <span className={classes.parameterControlText}>
-              Binary Threshold (max):
-            </span>
-            <Slider
-              min={0}
-              max={255}
-              className={classes.parameterControlSlider}
-              value={this.props.maxBinaryThreshold}
-              aria-labelledby="label"
-              onChange={this.props.onChangeMaxBinaryThreshold}
-            />
-            <span className={classes.parameterControlValue}>
-              {this.props.maxBinaryThreshold}
-            </span>
-          </div>
-          <div className={classes.parameterControl}>
-            <span className={classes.parameterControlText}>Gaussian Blur:</span>
-            <Slider
-              min={0}
-              max={20}
-              className={classes.parameterControlSlider}
-              value={this.props.gaussianBlur}
-              aria-labelledby="label"
-              onChange={this.props.onChangeGaussianBlur}
-            />
-            <span className={classes.parameterControlValue}>
-              {this.props.gaussianBlur}
-            </span>
-          </div>
-          <Button
-            onClick={onClickProcess}
-            className={classes.button}
-            variant="contained"
-            color="primary"
-          >
-            {' '}
-            Process Image{' '}
-          </Button>
-        </div>
-        <div className={classes.imageBox}>
-          <h1>Detected Circles</h1>
-          <div className={classes.image}>
-            {detectedImg ? (
-              <Image imgData={detectedImg} />
-            ) : (
-              loadingDetectedCircles && (
-                <CustomSpinner message="Detecting Circles..." />
-              )
-            )}
-          </div>
-          <div className={classes.parameterControl}>
-            <span className={classes.parameterControlText}>dp:</span>
-            <Slider
-              min={0}
-              max={10}
-              className={classes.parameterControlSlider}
-              value={this.props.dp}
-              aria-labelledby="label"
-              onChange={this.props.onChangeDp}
-            />
-            <span className={classes.parameterControlValue}>
-              {this.props.dp}
-            </span>
-          </div>
-          <div className={classes.parameterControl}>
-            <span className={classes.parameterControlText}>
-              Center distance (min):
-            </span>
-            <Slider
-              min={0}
-              max={1000}
-              className={classes.parameterControlSlider}
-              value={this.props.centerDistance}
-              aria-labelledby="label"
-              onChange={this.props.onChangeCenterDistance}
-            />
-            <span className={classes.parameterControlValue}>
-              {this.props.centerDistance}
-            </span>
-          </div>
-          <div className={classes.parameterControl}>
-            <span className={classes.parameterControlText}>Radius (min):</span>
-            <Slider
-              min={1}
-              max={300}
-              className={classes.parameterControlSlider}
-              value={this.props.minRadius}
-              aria-labelledby="label"
-              onChange={this.props.onChangeMinRadius}
-            />
-            <span className={classes.parameterControlValue}>
-              {this.props.minRadius}
-            </span>
-            <div className={classes.parameterControl}>
-              <span className={classes.parameterControlText}>
-                Radius (max):
-              </span>
-              <Slider
-                min={20}
-                max={400}
-                className={classes.parameterControlSlider}
-                value={this.props.maxRadius}
-                aria-labelledby="label"
-                onChange={this.props.onChangeMaxRadius}
-              />
-              <span className={classes.parameterControlValue}>
-                {this.props.maxRadius}
-              </span>
-            </div>
-          </div>
-          <Button
-            onClick={onClickDetect}
-            className={classes.button}
-            variant="contained"
-            color="primary"
-          >
-            {' '}
-            Detect Circles{' '}
-          </Button>
-        </div>
+        {this.generateImageBox(originalImg, 'Original', loading[0], classes)}
+
+        {this.generateImageBox(
+          processedImg,
+          'Processed',
+          loading[1],
+          classes,
+          paramsDataProcessing,
+          changeParams,
+          params
+        )}
+
+        {this.generateImageBox(
+          detectedImg,
+          'Detected Circles',
+          loading[2],
+          classes,
+          paramsDetectCircles,
+          changeParams,
+          params
+        )}
       </div>
     );
   }
