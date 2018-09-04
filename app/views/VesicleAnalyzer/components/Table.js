a46fc95101d573fe7dac8d006ad984d5dcca60b5
Hjörtur Hjartarson <hjoertur.hjartarson@daimler.com>
2018-09-04 17:58:06 +0200

    Created basic table for diameter data

---
+++ app/views/VesicleAnalyzer/components/Table.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { compose } from 'recompose';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import withLoadingSpinner from '../../../utils/with/loadingSpinner';
+import tableStyle from '../styles/tableStyle';
+
+class DiameterTable extends Component<Props> {
+  render() {
+    const { classes, data } = this.props;
+
+    return (
+      <div className={classes.tableContainer}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Cell</TableCell>
+              <TableCell numeric>Diameter</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((diameter, idx) => (
+              <TableRow key={idx}>
+                <TableCell>{idx+1}</TableCell>
+                <TableCell numeric>{diameter}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default compose(
+  withStyles(tableStyle),
+  withLoadingSpinner('Loading data')
+)(DiameterTable);
