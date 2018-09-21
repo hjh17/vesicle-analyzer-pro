5ad122b04fa992a0ff3227a4c8d37f5ce09d28b6
Hjörtur Hjartarson <hjoertur.hjartarson@daimler.com>
2018-09-21 17:42:27 +0200

    fix: opencv dependency

--- pysrc/setup.py
+++ pysrc/setup.py
@@ -5,5 +5,5 @@ setup(
     version='1.0.0',
     description='Analyzes circles on pack of .tif images',
     author='Hjortur Hjartarson',
-    packages=find_packages(), install_requires=['zerorpc==0.6.1', 'cv2', 'numpy', 'gevent==1.2.2']
+    packages=find_packages(), install_requires=['zerorpc==0.6.1', 'opencv-python-headless==3.4.2.17', 'numpy', 'gevent==1.2.2']
 )
